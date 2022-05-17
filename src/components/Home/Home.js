c98c60fecc04f466312a69366f709b823a7eb41f
Daniel Ian Reyes <rdanielian1@gmail.com>
2022-05-16 19:02:43 -0700

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+import Class from "../Class/Class";
+import "../Home/Home.css";
+
+function Home(props) {
+    
+    const [value, setValue] = useState('');
+    const [favoriteClasses, setClasses] = useState([]);
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (!favoriteClasses.includes(value)) {
+            setClasses(favoriteClasses.concat(value))
+            setValue('');
+        }
+        console.log(favoriteClasses);
+    }
+
+
+
+    return(
+        <div>
+            <h1 id="h1">Favorite Class List</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Add Favorite Class</label>
+                <input type="text" value={value} onChange={handleChange}></input>
+                <button typ="submit">Add Class!</button>
+            </form>
+            <div class="my-classes">
+                {favoriteClasses.map((favClass) =>
+                    <Class name={favClass} key={favClass}></Class>
+                )}
+            </div>
+        </div>
+
+    )
+}
+
+export default Home;
